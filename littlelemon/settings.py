dd5a8c433102447a9f17c77ec6c5a12393a9ebe6
Michael Vela <mvela1@outlook.com>
2023-07-24 06:42:03 -0700

    switch to djoser for authentication

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
     'restaurant',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -129,3 +132,16 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES' : [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+}
+
+DJOSER={"USER_ID_FIELD":"username"}
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -8,8 +8,7 @@ router.register(r'booking/tables', views.BookingViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', views.IndexView.as_view(), name='index'),
     path('menu/', views.MenuItemView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
-    path('', views.IndexView.as_view(), name='index'),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
