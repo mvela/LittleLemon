a76419f8e3486394945fef65647b7b8b2b002d11
Michael Vela <mvela1@outlook.com>
2023-07-24 18:43:53 -0700

    add model and view tests

---
+++ restaurant/tests/test_views.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+from ..models import Menu
+from django.urls import reverse
+from ..serializers import MenuSerializer
+from django.http import JsonResponse
+import json
+from decimal import Decimal
+
+class DecimalJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return str(obj)
+        return super().default(obj)
+    
+class MenuViewTest(TestCase):
+    def setUp(self):
+        Menu.objects.create(name="Gelato", price=4.99, inventory=64)
+        Menu.objects.create(name="Pizza Slice", price=5.99, inventory=44)
+        Menu.objects.create(name="Soda", price=1.59, inventory=33)
+
+    def test_menu_list_view(self):
+        url = reverse('menu')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        
+        queryset = Menu.objects.all()
+        expected_data = [{'id':item.id,'name':item.name,'price':item.price,'inventory':item.inventory} for item in queryset]
+        expected_json_response = json.dumps(expected_data, cls=DecimalJSONEncoder)
+
+        response_data = json.loads(response.content)
+        normalized_response_content = json.dumps(response_data, cls=DecimalJSONEncoder)
+
+        self.assertEqual(normalized_response_content, expected_json_response)
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -9,6 +9,6 @@ router.register(r'booking/tables', views.BookingViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('', views.IndexView.as_view(), name='index'),
-    path('menu/', views.MenuItemView.as_view()),
+    path('menu/', views.MenuItemView.as_view(), name="menu"),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
